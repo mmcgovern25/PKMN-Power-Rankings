f2ed4bacd68cdeec4070b9b830b0b349bfdd165c
Matthew McGovern <mattmcg37@gmail.com>
2024-05-22 14:42:19 -0300

    intro the new index

--- intro.js
+++ intro.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('.pokeball__button').addEventListener('click', function() {
 
     setTimeout(function() {
-      window.location.href = 'index.html';
+      window.location.href = 'pokedex.html';
     }, 500);
   });
 });
